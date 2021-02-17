d09ddd2af90b1b60bce2a6e0ed88bdba92e821a8
student <tien.ho2@ngc.com>
2021-02-17 23:02:06 +0000

    version 1

---
+++ src/main/java/com/example/MavenProject1/MavenProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.MavenProject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MavenProject1/MavenProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MavenProject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
